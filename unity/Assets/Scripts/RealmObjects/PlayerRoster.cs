27d2c38443b05607380862fc81d80954d1e23ce5
Nic Raboy <nicolas.raboy@mongodb.com>
2021-05-26 16:06:35 -0700

    update realm mappings to reflect new collection data models

--- unity/Assets/Scripts/RealmObjects/PlayerRoster.cs
+++ unity/Assets/Scripts/RealmObjects/PlayerRoster.cs
@@ -1,36 +1,47 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
-using UnityEngine;
 using Realms;
 using MongoDB.Bson;
 
-public class PlayerRoster : RealmObject
-{
+[System.Serializable]
+public class PlayerRoster : RealmObject {
+
     [PrimaryKey]
     [MapTo("_id")]
     public ObjectId? Id { get; set; }
+
+    [MapTo("lastUpdateDt")]
+    public DateTimeOffset? LastUpdateDt { get; set; }
+
     [MapTo("playerId")]
     public string PlayerId { get; set; }
+
     [MapTo("roster")]
     public IList<PlayerRoster_roster> Roster { get; }
-    // [MapTo("ts")]
-    // public DateTimeOffset? Ts { get; set; }
+
 }
 
-public class PlayerRoster_roster : EmbeddedObject
-{
+public class PlayerRoster_roster : EmbeddedObject {
+
     [MapTo("abilities")]
     public int? Abilities { get; set; }
+
     [MapTo("characterId")]
     public int? CharacterId { get; set; }
-    [MapTo("gear_tier")]
+
+    [MapTo("gearTier")]
     public int? GearTier { get; set; }
+
     [MapTo("level")]
     public int? Level { get; set; }
-    [MapTo("redstars")]
-    public int? Redstars { get; set; }
+
+    [MapTo("redStars")]
+    public int? RedStars { get; set; }
+
     [MapTo("shards")]
     public int? Shards { get; set; }
+
     [MapTo("stars")]
     public int? Stars { get; set; }
+    
 }
